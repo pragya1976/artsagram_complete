8abdd6ffcec109be9f7a2706c02e395a333e9d60
pragya1976 <palpragya688@gmail.com>
2023-07-08 14:47:52 +0530

    initial commit

---
+++ client/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  esbuild: {
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
+});
